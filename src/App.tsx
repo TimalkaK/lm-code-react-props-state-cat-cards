345715b140e0360a5c0e066a15889610c27e0fee
TimalkaK <timalkamk@gmail.com>
2023-09-04 14:59:13 +0100

    feat: Completed Activity 7

--- src/App.tsx
+++ src/App.tsx
@@ -83,16 +83,24 @@ function App(): JSX.Element {
 				species: 'Tiny Cat',
 				favFoods: ['milk'],
 				birthYear: 2021,
+			},
+			{
+				name: "Captain Catface",
+				species: "Sea Cattain",
+				favFoods: ["fish, rum"],
+				birthYear: 2016,
 			}
 	]
 	);
 
-	console.log("Our pretties 😻: ", cats);
+	const catCount = cats.length;
+
+	console.log("Our pretties 😻: ", catCount);
 
 	return (
 		<>
 			<Navbar />
-			<Header />
+			<Header catCount={catCount}></Header>
 
 			<main>
 				<div className='cards__wrapper'>
